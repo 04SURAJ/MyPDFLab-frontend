81d3aa5abac7219e5742f3f6d2fe7bfd3d9a75ee
Suraj coder <surajjanjal1234@gmail.com>
2025-12-26 18:22:56 +0530

    api.js update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mypdflab-backend-production.up.railway.app", // make sure this matches your backend route
+  baseURL: "https://mypdflab-backend-production.up.railway.app/api", // make sure this matches your backend route
 });
 
 export default api;
